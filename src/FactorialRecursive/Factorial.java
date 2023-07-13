b1c2c4159473e9871a7ff50b6f290a1090d5cbd2
suleymancansimsek <suleymancansimsek@gmail.com>
2023-07-13 13:07:37 +0300

    recursive factorial function added

---
+++ src/FactorialRecursive/Factorial.java
@@ -0,0 +1,24 @@
+package FactorialRecursive;
+/*
+ this class shows that how can we make recursive factorial method
+
+ remainder for factorial:
+  n! = n*(n-1)*(n-2)*...* 1
+  ex: 5! = 5*4*3*2*1 = 120
+ */
+public class Factorial {
+    //test method
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println(factorial(5));
+    }
+
+    //factorial method with recursive algorithm
+    public static int factorial(int n){
+        //base case
+        if (n <= 0){
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
